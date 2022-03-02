7a43438334b56ed2cf46d513481ca64ebc75739b
hhongisred <hhong536301@163.com>
2022-03-02 12:27:06 +0800

    add 2 files

---
+++ count.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+extern int count(int array[], int n);
+
+int count() {
+    for (int i = 0; 
+    i < n;
+    i ++) {
+        if (A[i]%2 == 0)
+        count ++;
+    }
+    std::cout << "The number is: " << i << std::endl;
+    return count;
+}
