bc596cc0ccc7e652d44676a68164f36399021c9c
hhongisred <100658207+hhongisred@users.noreply.github.com>
2022-03-15 12:31:41 +0800

    workshop3

---
+++ function-1-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void print_class(std::string courses[4], std::string students[], int report_card[][4], int nstudents)
+{
+    for(int i = 0;  i < 4;i ++)
+    {
+        for 
+    }
+}
