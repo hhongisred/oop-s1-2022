1ac5b91389bdd1828322bd823c35aa4ea730d2a6
hhongisred <100658207+hhongisred@users.noreply.github.com>
2022-03-17 07:55:20 +0800

    prac_exam
    there are my prac exam

---
+++ function-1-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void printer(int array[10][10])
+
+{
+    for(int i = 0; i<10; i++)
+    {
+        for(int j = 0;j<10;j++)
+        {
+            std::cout << array [i][j] << " ";
+        }
+
+        std::cout<<std::endl;
+    }
+}
